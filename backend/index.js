fd214c18980382ccc153ad2a243ece7564d9b432
MubashirAnwaar <mubashiranwaar681@gmail.com>
2025-03-02 15:58:06 +0500

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,32 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-})
+// Enable CORS for frontend origin
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//   })
+// );
+
+app.use(express.json());
+// const app = express();
+
+const port = process.env.PORT || 3000;
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

4958cb9181e6a05d2381f08f73bd4bd53fba180d
MubashirAnwaar <mubashiranwaar681@gmail.com>
2025-03-02 11:19:21 +0500

    add pages and routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import Home from "./pages/Home";
+import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewsArticle from "./pages/NewsArticle";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/news" element={<NewsArticle />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/pages/auth/api/auth.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const loginUser = async (data) => {
-  const response = await axios.post("http://localhost:3000/api/users/login", data);
-  return response.data;
-};
